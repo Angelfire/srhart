f7818dca6a19806f178a9d49ae89188c2dbbffb3
Andres Bedoya <andresbedoyag@Andress-MacBook-Pro.local>
2023-01-25 10:29:25 -0500

    Add new me photo, Add 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -7,14 +7,14 @@ export default function Custom404() {
   return (
     <Layout
       canonical="404"
-      description="404 - What are you looking for? "
+      description="404 - What are you looking for?"
       keywords="404, not found"
       title="404 - Not found"
     >
       <div className="flex flex-col mb-12 mx-8 sm:container">
         <h1 className={`${ibm.className} text-9xl text-center`}>404</h1>
         <p className={`${ibm.className} text-6xl`}>Oops!</p>
-        <p className={`${ibm.className} text-6xl`}>We can't seem to find the page you're looking for.</p>
+        <p className={`${ibm.className} text-6xl`}>We can not seem to find the page you are looking for.</p>
       </div>
     </Layout>
   )
