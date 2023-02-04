3df0b3ae77ab6864e9493081c5a191971a81d97f
Andres Bedoya <andresbedoyag@Andress-MacBook-Pro.local>
2023-02-04 09:31:23 -0500

    Me changes

--- components/Me.tsx
+++ components/Me.tsx
@@ -5,8 +5,8 @@ export default function Me() {
   return (
     <div className="flex justify-between mb-8 mx-8 sm:container">
       <div className="flex flex-col">
-        <span className="text-6xl font-extrabold">I am</span>
-        <h1 className="from-pink-600 bg-clip-text bg-gradient-to-l text-transparent to-blue-600 text-6xl font-extrabold py-2">Andrés Bedoya</h1>
+        <span className="text-4xl md:text-6xl font-extrabold">I am</span>
+        <h1 className="from-pink-600 bg-clip-text bg-gradient-to-l text-transparent to-blue-600 text-4xl md:text-6xl font-extrabold py-2">Andrés Bedoya</h1>
         <div className="mt-3 inline-flex items-center gap-2">
           <span className="relative flex h-2 w-2">
             <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-emerald-400 opacity-75" />
@@ -16,11 +16,11 @@ export default function Me() {
             Currently in Medellín, Colombia <span className="mr-1">&#127464;&#127476;</span>
           </span>
         </div>
-        <p className="font-medium mt-2 text-lg">
-          JavaScript Software Engineer with an interest in fund investing, personal finance and Open Source.
+        <p className="font-medium mt-2 text-base md:text-lg">
+          JavaScript Software Engineer with an interest in fund investing, personal finance, web development and Open Source.
           See my <Link className="text-blue-600 hover:underline" href="https://read.cv/anbedoyag" rel="noopener noreferrer" target="_blank">Curriculum Vitae</Link>.
         </p>
-        <p className="font-medium mt-2 text-lg">
+        <p className="font-medium mt-2 text-base md:text-lg">
           Blogger Evangelist at{' '}
         <Link
           aria-label="Velocidad de Escape blog"
