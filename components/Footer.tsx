01729e001fa4880bd7de4001e0d2b3453db574e3
Angelfire <mortiis.angel@gmail.com>
2023-02-12 11:23:34 -0500

    Fix social icons color and Order footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ import { ThemeToggle } from "@/components/ThemeToggle"
 export default function Footer() {
   return (
     <footer className="border-t border-t-slate-200 py-2 dark:border-t-slate-700">
-      <div className="flex flex-col items-center justify-between gap-2 sm:container sm:flex-row sm:gap-0">
+      <div className="flex flex-col-reverse items-center justify-between gap-2 sm:container sm:flex-row sm:gap-0">
         <p className="flex items-center gap-1 text-sm">
           Made with <strong>&#60; &#47;&#62;</strong> and{" "}
           <Beer className="w-4" /> in Colombia
--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -16,22 +16,22 @@ export function ThemeToggle() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" size="sm">
-          <Sun className="hover:text-slate-900 dark:text-slate-400 dark:hover:text-slate-100" />
+          <Sun className="hover:text-slate-900 dark:text-slate-100 dark:hover:text-slate-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" forceMount>
         <DropdownMenuItem onClick={() => setTheme("light")}>
-          <Sun className="mr-2 h-4 w-4" />
-          <span>Light</span>
+          <Sun className="mr-2 h-4 w-4 dark:text-slate-100" />
+          <span className="dark:text-slate-100">Light</span>
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("dark")}>
-          <Moon className="mr-2 h-4 w-4" />
-          <span>Dark</span>
+          <Moon className="mr-2 h-4 w-4 dark:text-slate-100" />
+          <span className="dark:text-slate-100">Dark</span>
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("system")}>
-          <Laptop className="mr-2 h-4 w-4" />
-          <span>System</span>
+          <Laptop className="mr-2 h-4 w-4 dark:text-slate-100" />
+          <span className="dark:text-slate-100">System</span>
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
